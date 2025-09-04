a897dd0a1bbc86ee22b372166af9a493a73537f8
Keshav Dutt Kaushik <keshav.duttkaushik@cognizant.com>
2025-09-04 09:38:39 +0530

    solved lecture 66 while loop challenge

--- src/main/java/org/codemxde/section_6_mastering_conditional_logic_loops/lecture_66/WhileLoopChallenge.java
+++ src/main/java/org/codemxde/section_6_mastering_conditional_logic_loops/lecture_66/WhileLoopChallenge.java
@@ -4,5 +4,20 @@ public class WhileLoopChallenge {
 
     public static void main(String[] args) {
         // to be done later
+        int number = 5;
+        int evenNumbers = 0;
+        while (number <= 20 && evenNumbers < 5) {
+            if (isEvenNumber(number)) {
+                evenNumbers++;
+                System.out.println("Even: " + number);
+            } else {
+                System.out.println("Odd: " + number);
+            }
+            number++;
+        }
+    }
+
+    public static boolean isEvenNumber(int number) {
+        return number % 2 == 0;
     }
 }
